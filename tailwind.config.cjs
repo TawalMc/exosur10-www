bd8289dab27174a687a2130849e3c3f3c1b0b11a
KenpachiZaraki <tawaliou.alao@erugis.fr>
2023-04-30 01:07:30 +0100

    update some pages to have something that suits esosur10 needs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
+        // sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ['Sen', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: [require("@tailwindcss/typography")],
-};
+  plugins: [require('@tailwindcss/typography')]
+}
